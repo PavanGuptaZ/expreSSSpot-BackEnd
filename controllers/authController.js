4ba445babed0711bcce7de677bb64959f02b559c
PavanGuptaZ <uppalagupta456@gmail.com>
2023-11-10 19:10:47 +0530

    231110-fix4

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,8 +27,8 @@ const login = asyncHandler(async (req, res) => {
     res.cookie('jwt', refreshToken, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        // sameSite: 'None',
-        // secure: true,
+        sameSite: 'None',
+        secure: true,
     })
 
     res.json({ status: "ok", user: { ...rem, accessToken } })
